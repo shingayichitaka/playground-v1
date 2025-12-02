7ec194b565eb25db4152dfe2653349a41f2ad0eb
shingayichitaka <shingayidambudzochitaka@gmail.com>
2025-12-02 23:03:23 +0100

    completed assignment: type conversion and coercion

--- scripts/assignments.js
+++ scripts/assignments.js
@@ -14,3 +14,9 @@ if (population > 33) {
     `${country}'s population is ${population - 33} million above average`
   );
 }
+
+console.log("9" - "5"); // -> ?
+console.log("19" - "13" + "17"); // -> ?
+console.log("19" - "13" + 17); // -> ?
+console.log("123" < 57); // -> ?
+console.log(5 + 6 + "4" + 9 - 4 - 2); // -> ?
