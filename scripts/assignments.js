bd7ea5efa1de16e03bf5eeffd8e2f16b0d3e80dd
shingayichitaka <shingayidambudzochitaka@gmail.com>
2025-12-02 22:40:51 +0100

    completed assignment: data types

--- scripts/assignments.js
+++ scripts/assignments.js
@@ -7,3 +7,13 @@ const population = 15;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+const isIsland = false;
+const language = "Shona";
+
+console.log(`isIsland is type: ${typeof isIsland} and value: ${isIsland}`);
+console.log(
+  `population is type: ${typeof population} and value: ${population}`
+);
+console.log(`country  is type: ${typeof country} and value: ${country}`);
+console.log(`language  is type: ${typeof language} and value: ${language}`);
