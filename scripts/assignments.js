46cd07b447e476ce37f28472a29a92d5618f83d7
shingayichitaka <shingayidambudzochitaka@gmail.com>
2025-12-02 23:01:08 +0100

    completed assignment: strings and template literals

--- scripts/assignments.js
+++ scripts/assignments.js
@@ -13,4 +13,3 @@ console.log(population > 6);
 console.log(population < 33);
 
 const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
-console.log(description);
