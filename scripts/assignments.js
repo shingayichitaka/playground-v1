8c934c7b0ad16586111890abf7626bd7f9cd7f72
shingayichitaka <shingayidambudzochitaka@gmail.com>
2025-12-02 23:00:44 +0100

    completed assignment: basic operators

--- scripts/assignments.js
+++ scripts/assignments.js
@@ -6,3 +6,11 @@ let population = 15;
 
 const isIsland = false;
 const language = "Shona";
+
+console.log(`Half population: ${population / 2}`);
+console.log(++population);
+console.log(population > 6);
+console.log(population < 33);
+
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+console.log(description);
