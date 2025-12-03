247488952c20325bbe18dcbc07c498d400ec351f
shingayichitaka <shingayidambudzochitaka@gmail.com>
2025-12-03 09:20:00 +0100

    cleared console logs

--- scripts/assignments.js
+++ scripts/assignments.js
@@ -28,33 +28,33 @@ if (population > 33) {
 //   console.log(`No borders!`);
 // }
 
-if (language === "English" && population < 50 && isIsland === false) {
-  console.log(`⭐ You should live in ${country}! ⭐`);
-} else {
-  console.log(`😭 ${country} does not meet your criteria! 😭`);
-}
+// if (language === "English" && population < 50 && isIsland === false) {
+//   console.log(`⭐ You should live in ${country}! ⭐`);
+// } else {
+//   console.log(`😭 ${country} does not meet your criteria! 😭`);
+// }
 
-switch (language) {
-  case "Chinese":
-  case "Mandarin":
-    console.log(`MOST number of native speakers!`);
-    break;
-  case "Spanish":
-    console.log(`2nd place in number of native speakers`);
-    break;
-  case "English":
-    console.log(`3rd place`);
-    break;
-  case "Hindi":
-    console.log(`Number 4`);
-    break;
-  case "Arabic":
-    console.log(`5th most spoken language`);
-    break;
-  default:
-    console.log(`Great language too :D`);
-}
+// switch (language) {
+//   case "Chinese":
+//   case "Mandarin":
+//     console.log(`MOST number of native speakers!`);
+//     break;
+//   case "Spanish":
+//     console.log(`2nd place in number of native speakers`);
+//     break;
+//   case "English":
+//     console.log(`3rd place`);
+//     break;
+//   case "Hindi":
+//     console.log(`Number 4`);
+//     break;
+//   case "Arabic":
+//     console.log(`5th most spoken language`);
+//     break;
+//   default:
+//     console.log(`Great language too :D`);
+// }
 
-console.log(
-  `${country}'s population is ${population > 33 ? "above" : "below"} average!`
-);
+// console.log(
+//   `${country}'s population is ${population > 33 ? "above" : "below"} average!`
+// );
